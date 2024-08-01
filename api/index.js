ce54e748a21cbbedd548894351c36be96c960e29
Bhupesh Rajendra Chaudhari <bhupeshchaudhari19@gmail.com>
2024-08-01 20:56:20 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
+//MongoDB connection
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -12,8 +14,11 @@ mongoose
     console.log(err);
   });
 
+//server
 const app = express();
-
 app.listen(3000, () => {
   console.log("server listening on port 3000..");
 });
+
+//Routes
+app.use("/api/user", userRoutes);
