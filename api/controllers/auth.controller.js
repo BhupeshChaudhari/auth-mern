630b3e4abf616dd4389bae9f77c1798708533d79
Bhupesh Rajendra Chaudhari <bhupeshchaudhari19@gmail.com>
2024-08-02 14:46:02 +0530

    create sign in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import User from "../models/user.model.js";
-import bccryptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
+//SignUp
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bccryptjs.hashSync(password, 10);
@@ -13,6 +17,35 @@ export const signup = async (req, res, next) => {
       success: true,
       message: "User created Succesfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//SignIn
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password"));
+    }
+    const { password: hashPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date(Date.now() + 3600000));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("token", token, { httpsOnly: true, expires: expiryDate })
+      .status(200)
+      .json({
+        data: rest,
+        token: token,
+      });
   } catch (error) {
     next(error);
   }
