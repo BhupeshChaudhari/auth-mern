401c8a2338a265e1547ec01c5f9cbb6111f432ab
Bhupesh Rajendra Chaudhari <bhupeshchaudhari19@gmail.com>
2024-08-06 22:36:51 +0530

    update home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-indigo-800 text-2xl mt-10 flex justify-center items-center">
+        Welcome,&nbsp;
+        <span className="text-indigo-950 font-bold">
+          {currentUser.data.username}
+        </span>
+      </h1>
     </div>
   );
 };
